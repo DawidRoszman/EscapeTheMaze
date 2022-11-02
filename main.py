1b50748bd1deb0b5c5b5b650a3c44a0b50b72b2e
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2022-11-02 21:39:47 +0100

    move_player function is now in player class

--- main.py
+++ main.py
@@ -5,20 +5,6 @@ def print_array(array):
         print("\n")
 
 
-def move_player(array, move_dir, player_position) -> [list, tuple[int, int]]:
-    new_player_pos = (player_position[0] + move_dir[0], player_position[1] + move_dir[1])
-    x = array[new_player_pos[0]][new_player_pos[1]]
-    if x == 0:
-        array[new_player_pos[0]][new_player_pos[1]] = 2
-        array[player_position[0]][player_position[1]] = 0
-    if x == 3:
-        print("Congrats you won")
-        return False
-    if x == 1:
-        return array, player_position
-    return array, new_player_pos
-
-
 def get_input() -> tuple[int, int]:
     x = input("Choose direction(['w' - up] ['s' - down] ['d' - right] ['a' - left]):  ")
     if x == "w":
@@ -38,13 +24,28 @@ class Player:
     def __init__(self, player_pos):
         self.player_pos = player_pos
 
+    def move_player(self, array, move_dir, player_position) -> [list, tuple[int, int]]:
+        new_player_pos = (player_position[0] + move_dir[0], player_position[1] + move_dir[1])
+        x = array[new_player_pos[0]][new_player_pos[1]]
+        if x == 0:
+            array[new_player_pos[0]][new_player_pos[1]] = 2
+            array[player_position[0]][player_position[1]] = 0
+        if x == 3:
+            print("Congrats you won")
+            return False
+        if x == 1:
+            return array
+        self.change_player_position(new_player_pos)
+        return array
+
     def change_player_position(self, new_player_pos):
         self.player_pos = new_player_pos
 
     def get_current_position(self):
         return self.player_pos
-def main():
 
+
+def main():
     player = Player((1, 1))
 
     lab = [
@@ -62,10 +63,9 @@ def main():
         print("Turns: ", turns_left)
         print_array(lab)
         current_move_dir = get_input()
-        if not move_player(lab, current_move_dir, player.get_current_position()):
+        if not player.move_player(lab, current_move_dir, player.get_current_position()):
             break
-        lab, new_player_position = move_player(lab, current_move_dir, player.get_current_position())
-        player.change_player_position(new_player_position)
+        lab = player.move_player(lab, current_move_dir, player.get_current_position())
         turns_left -= 1
 
     if turns_left <= 0:
